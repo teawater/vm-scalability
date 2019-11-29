teawater/vm-scalability

commit 4b12da9f8a403ea5a31f578dc90805e60bf8dcd5

usemem: rename reps to opt_repeat
Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>

usemem.c
@@ -76,7 +76,7 @@ unsigned long *buffer;
 int sleep_secs = 0;
 time_t runtime_secs = 0;
 struct timeval start_time;
-int reps = 1;
+int opt_repeat = 1;
 int do_mlock = 0;
 int do_getchar = 0;
 int opt_randomise = 0;
@@ -462,7 +462,7 @@ void delay(unsigned long delay, unsigned long *p, unsigned long idx, int read)
 
 static unsigned long do_rw_once(unsigned long *p, unsigned long bytes,
 				struct drand48_data *rand_data, int read,
-				int *rep, int reps)
+				int *rep)
 {
 	unsigned long i;
 	unsigned long m = bytes / sizeof(*p);
@@ -497,7 +497,7 @@ static unsigned long do_rw_once(unsigned long *p, unsigned long bytes,
 
 		if (!(i & 0xffff) && runtime_exceeded()) {
 			if (rep)
-				*rep = reps;
+				*rep = opt_repeat;
 			break;
 		}
 	}
@@ -550,13 +550,13 @@ unsigned long do_unit(unsigned long bytes, struct drand48_data *rand_data,
 	if (opt_write_signal_read)
 		buffer = p;
 
-	for (rep = 0; rep < reps; rep++) {
+	for (rep = 0; rep < opt_repeat; rep++) {
 		if (rep > 0 && !quiet) {
 			printf(".");
 			fflush(stdout);
 		}
 
-		rw_bytes += do_rw_once(p, bytes, rand_data, opt_readonly, &rep, reps);
+		rw_bytes += do_rw_once(p, bytes, rand_data, opt_readonly, &rep);
 
 		if (msync_mode) {
 			if ((msync(p, bytes, msync_mode)) == -1) {
@@ -702,13 +702,13 @@ long do_units(void)
 		for (i = 0; i < nptr; i++) {
 			int rep;
 
-			for (rep = 0; rep < reps; rep++) {
+			for (rep = 0; rep < opt_repeat; rep++) {
 				if (rep > 0 && !quiet) {
 					printf(".");
 					fflush(stdout);
 				}
 
-				rw_bytes += do_rw_once(ptrs[i], lens[i], &rand_data, 1, &rep, reps);
+				rw_bytes += do_rw_once(ptrs[i], lens[i], &rand_data, 1, &rep, opt_repeat);
 
 				if (msync_mode) {
 					if ((msync(ptrs[i], lens[i], msync_mode)) == -1) {
@@ -974,7 +974,7 @@ int main(int argc, char *argv[])
 			opt_detach = 1;
 			break;
 		case 'r':
-			reps = strtol(optarg, NULL, 10);
+			opt_repeat = strtol(optarg, NULL, 10);
 			break;
 		case 'R':
 			opt_randomise++;

commit 95081d4aeff089ff2891ae5e1334e475a634bc53

usemem.c: Add missing opt_repeat
Fix the following compiler error:
----------------------------------------
usemem.c:711:17: error: too many arguments to function ‘do_rw_once’
     rw_bytes += do_rw_once(ptrs[i], lens[i], &rand_data, 1, &rep, opt_repeat);
                 ^~~~~~~~~~
...
usemem.c:763:16: error: too many arguments to function ‘do_rw_once’
   unit_bytes = do_rw_once(buffer, opt_bytes, &rand_data, 1, NULL, 0);
                ^~~~~~~~~~
usemem.c:463:22: note: declared here
 static unsigned long do_rw_once(unsigned long *p, unsigned long bytes,
                      ^~~~~~~~~~
----------------------------------------

Fixes: 4b12da9("usemem: rename reps to opt_repeat")
Reported-by: kernel test robot <lkp@intel.com>
Signed-off-by: Xiao Yang <xiaox.yang@intel.com>

Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>

usemem.c
@@ -462,7 +462,7 @@ void delay(unsigned long delay, unsigned long *p, unsigned long idx, int read)
 
 static unsigned long do_rw_once(unsigned long *p, unsigned long bytes,
 				struct drand48_data *rand_data, int read,
-				int *rep)
+				int *rep, int opt_repeat)
 {
 	unsigned long i;
 	unsigned long m = bytes / sizeof(*p);
@@ -556,7 +556,7 @@ unsigned long do_unit(unsigned long bytes, struct drand48_data *rand_data,
 			fflush(stdout);
 		}
 
-		rw_bytes += do_rw_once(p, bytes, rand_data, opt_readonly, &rep);
+		rw_bytes += do_rw_once(p, bytes, rand_data, opt_readonly, &rep, opt_repeat);
 
 		if (msync_mode) {
 			if ((msync(p, bytes, msync_mode)) == -1) {